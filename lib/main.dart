5bbd5797df3ecfa425f9f181c8ad9c8f47edd170
Ahlam gomaa <129538648+Ahlamgomaa@users.noreply.github.com>
2025-02-19 15:35:58 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(const GreanGoApp());
+}
+class GreanGoApp extends StatelessWidget{
+  const GreanGoApp({super.key});
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false ,
+      home: Scaffold(
+      
+      ),
+    );
+  }
+
+}
